44ea049c23d102dc7060c7010a3dd93a9cb5fe68
Get-wy <47047064@qq.com>
2024-07-17 11:21:44 +0800

    2

--- api/testppai/ppai.go
+++ api/testppai/ppai.go
@@ -2,7 +2,7 @@
  * @Author: 王宇
  * @Date: 2024-07-17 10:52:08
  * @LastEditors: 王宇
- * @LastEditTime: 2024-07-17 11:04:11
+ * @LastEditTime: 2024-07-17 11:21:35
  * @FilePath: /wxcloudrun-wxcomponent/api/testppai/ppai.go
  * @Description:
  *
@@ -12,10 +12,12 @@ package testppai
 import (
 	"net/http"
 
+	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/log"
 	"github.com/gin-gonic/gin"
 )
 
 // GetTicketHandler 获取Ticket
 func ppHandler(c *gin.Context) {
+	log.Infof("======ppHandler===========")
 	c.JSON(http.StatusOK, gin.H{"message": "hello"})
 }
